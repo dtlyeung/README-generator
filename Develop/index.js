089ec5c86ba9192d72df8cab6dda3f0225ea66e9
Daniel <danieltlyeung@gmail.com>
2022-08-30 17:24:28 -0400

    Initial commit

---
+++ Develop/index.js
@@ -0,0 +1,19 @@
+// Required packages
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
+// Questions for user
+// Name
+// Github username
+
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
